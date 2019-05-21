7a4c8c47f0e034af448ecb9467dd42405758c52a
popmusic555 <zy3gprogramer@163.com>
2019-05-22 01:57:09 +0800

    数据表添加

---
+++ Client/ExcelConfig/export.bat
@@ -0,0 +1,11 @@
+@SET EXCEL_FOLDER=.\Excel
+@SET JSON_FOLDER=.\GameData
+@SET EXE=.\excel2json_120\Release\excel2json.exe
+
+@ECHO Converting excel files in folder %EXCEL_FOLDER% ...
+for /f "delims=" %%i in ('dir /b /a-d /s %EXCEL_FOLDER%\*.xlsx') do (
+    @echo   processing %%~nxi 
+    @CALL %EXE% --excel %EXCEL_FOLDER%\%%~nxi --json %JSON_FOLDER%\%%~ni.json --header 3 --array true
+)
+
+pause
